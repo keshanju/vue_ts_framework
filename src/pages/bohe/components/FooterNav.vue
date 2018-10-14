2b68488409ab1901b972456e713eb35b1d1d14d5
keshanju <15172400950@163.com>
2018-10-14 17:16:48 +0800

    ’footer_nav.vue组件‘

--- src/pages/bohe/components/FooterNav.vue
+++ src/pages/bohe/components/FooterNav.vue
@@ -1,5 +1,79 @@
 <template>
-    <div class="foot_nav">
-        footer
+    <div class="footer_nav">
+        <!-- 联系我们 -->
+        <div class="footer_connect">
+            <!-- 1 -->
+            <div class="wrap_of_links_bh">
+                <dl>
+                    <dt>关于我们</dt>
+                    <dt>关于我们</dt>
+                    <dt>关于我们</dt>
+                </dl>
+                <dl>
+                    <dt>网站导航</dt>
+                    <dt>网站导航</dt>
+                    <dt>网站导航</dt>
+                    <dt>网站导航</dt>
+                </dl>
+                <dl>
+                    <dt>帮助中心</dt>
+                    <dt>帮助中心</dt>
+                    <dt>帮助中心</dt>
+                    <dt>帮助中心</dt>
+                    <dt>帮助中心</dt>
+                </dl>
+            </div>
+            <!-- 2 -->
+            <div class="bottom_code">
+                <p class="ques_tit">关注微信公众号</p>
+                <div class="code">
+                    <img src="../images/code.png"></div>
+            </div>
+            <!-- 3 -->
+            <div class="bottom_contact">
+                <p class="ques_tit">联系我们</p>
+                <div class="contact_ways">
+                    <p>联系电话: 323323233</p>
+                    <p>官方QQ群: 35453234</p>
+                    <p>客服热线: 342323432432</p>
+                </div>
+                <a href="javascript:;" class="btn_common">联系客服</a>
+            </div>
+        </div>
+        <!-- 客户端下载 -->
+        <div class="footer_download">
+            <div class="load_left">
+                <p class="ques_tit">下载客户端</p>
+                <ul>
+                    <li>
+                        <a href="javascript:;"><i class="icon_windows02"></i>Windows</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="icon_android02"></i>Android</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="icon_mac02"></i>Mac</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="icon_iphone02"></i>iPhone / iPad</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="load_right">
+                <p>这里是网站备案号@上海XXXXXXXXX科技公司</p>
+                <ul>
+                    <li>
+                        <a href="javascript:;"><i class="icon_facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="icon_twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="javascript:;"><i class="icon_youtubu"></i></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
