05733fe99fe96ac62b1199a0f096db18e11e7168
keshanju <15172400950@163.com>
2018-10-14 15:50:08 +0800

    ‘实例演示’

--- src/pages/bohe_app/test.ts
+++ src/pages/bohe_app/test.ts
@@ -3,7 +3,11 @@ import Vuetity from 'vuetify';
 import Vue from 'vue';
 Vue.use(Vuetity);
 class Abb extends Vue {
-
+    public drawer: boolean = true;
+    public drawerRight: boolean = true;
+    public right: null = null
+    public left: null = null
+    public source: string = ''
 };
 //
 new Abb().$mount('#inspire');
